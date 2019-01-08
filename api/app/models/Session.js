10bdf617a588ab8e8ed4a8ef9d7816bcb09c2d1f
Luke Schuyler <lukeschuyler1@gmail.com>
2019-01-07 22:26:18 -0800

    updated session model to async await class

--- api/app/models/Session.js
+++ api/app/models/Session.js
@@ -1,48 +1,52 @@
 const { bookshelf } = require('../db/database');
 
-const Session = bookshelf.Model.extend({
-  tableName: 'session',
-  products: function() { return this.belongsToMany('Product').through('SessionLineItem') },
-  user: function() { return this.belongsTo('User') },
-  session_type: function() { return this.belongsTo('SessionType') },
-  items: function() { return this.hasMany('SessionLineItem') }
-}, {
-  getAll() {
-    return this.forge()
-    .fetchAll({withRelated: ['products', 'user', 'session_type'], require: true})
-    .then(sessions => sessions)
-    .catch(error => error)
-  },  
-  getAllByType(criteria) {
-    return this.where(criteria)
-    .fetchAll({withRelated: ['products', 'user'], require: true})
-    .then(sessions => sessions)
-    .catch(error => error)
-  },
-  getOne(id) {
-    return this.forge({id})
-    .fetch({withRelated: ['products', 'user', 'items'], require: true})
-    .then(session => session)
-    .catch(error => error)
-  },
-  addSession(newSession) {
-    return this.forge(newSession)
-    .save()
-    .then(session => session)
-    .catch(error => error)
-  },
-  deleteSession(id) {
-    return this.forge({id})
-    .destroy()
-    .then(session => session)
-    .catch(error => error)
-  },
-  editSession(id, edits) {
-    return this.where({id})
-    .save(edits, {method: 'update'})
-    .then(session => session)
-    .catch(error => error)
+class Session extends bookshelf.Model {
+
+  get tableName() { return 'session'; }
+  get dependents() { return ['session'] }
+
+  products() { return this.belongsToMany('Product').through('SessionLineItem') }
+  user() { return this.belongsTo('User') }
+  session_type() { return this.belongsTo('SessionType') }
+  items() { return this.hasMany('SessionLineItem') }
+
+  static async getAll() {
+    let [ err, sessions ] = await to(this.forge().fetchAll({withRelated: ['products', 'user', 'session_type'], require: true}));
+    if (err) return err;
+    return sessions;
   }
-});
+  
+  static async getAllByType(criteria) {
+    let [ err, sessions ] = await to(this.where(criteria).fetchAll({withRelated: ['products', 'user'], require: true}));
+    if (err) return err;
+    return sessions;
+  }
+
+  static async getOne(id) {
+    let [ err, session ] = await to(this.forge({id}).fetch({withRelated: ['products', 'user', 'items'], require: true}));
+    if (err) return err;
+    return session;
+  }
+
+  static async create(newItem) {
+    let [ err, item ] = await to(this.forge(newItem).save());
+    if (err) return err;
+    return item;
+  }
+
+  static async destroy(id) {
+    let [ err, item ] = await to(this.forge({id}).destroy());
+    if (err) return err;
+    return item;
+  }
+
+  static async update(id, edits) {
+    console.log(edits)
+    let [ err, items ] = await to(this.where(id).save(edits, {method: 'update'}));
+    console.log(err)
+    if (err) return err;
+    return items;
+  }
+}
 
 module.exports = bookshelf.model('Session', Session);
